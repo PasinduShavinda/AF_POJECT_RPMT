f7302469bbae21f6772890c5c89569bc7fd4a8c2
Sugandhi <sugandhi.kalansooriya@gmail.com>
2022-04-06 01:31:30 +0530

    DB connection creation

--- RPMT-backend/server.js
+++ RPMT-backend/server.js
@@ -1,1 +1,36 @@
-// server.js files
+const express =require('express');
+const mongoose =require('mongoose')
+const bodyParser=require('body-parser');
+const cors = require('cors')
+const app =express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+const PORT =8003;
+
+
+
+const DB_URL=`mongodb+srv://afProject2022:af2022proj12A@afprojectcluster.t6kdd.mongodb.net/Research_Management_DB?retryWrites=true&w=majority`;
+mongoose.connect(DB_URL)
+.then(()=>{
+    console.log('DB connected')
+    
+})
+.catch((err)=>console.log('DB connection error',err))
+
+app.listen(PORT,()=>{
+
+    console.log(`App is runnning on ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
