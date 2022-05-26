0d750e64f2d691c37bc8053175315eb4e8c6ccb3
Your Name <udarasandaruwan96@gmail.com>
2022-05-26 16:28:59 +0530

    create add student

--- RPMT-frontend/src/components/UD_Student/UD_AddStudent.js
+++ RPMT-frontend/src/components/UD_Student/UD_AddStudent.js
@@ -63,18 +63,18 @@ const UD_AddStudent = (props) => {
 
   return (
     <>
-      <div className="ud_add_Supplier_Form1">
-        <div className="ud_add_Supplier_title"> Add Student Details</div>
+      <div className="ud_add_Student_Form1">
+        <div className="ud_add_Student_title"> Add Student Details</div>
 
-        <form className="ud_add_Supplier_form2">
-          <div className="ud_add_Supplier_Photo"></div>
+        <form className="ud_add_Student_form2">
+          <div className="ud_add_Student_Photo"></div>
 
-          <label className="ud_edit_add_Supplier_name1">
+          <label className="ud_edit_add_Student_name1">
             <b>FirstName</b>
           </label>
           <br></br>
           <input
-            className="ud_edit_add_Supplier_box1"
+            className="ud_edit_add_Student_box1"
             type="text"
             value={firstName}
             required
@@ -84,11 +84,11 @@ const UD_AddStudent = (props) => {
           />
           <br></br>
 
-          <label className="ud_edit_add_Supplier_name2">
+          <label className="ud_edit_add_Student_name2">
             <b>LastName</b>
           </label>
           <input
-            className="ud_edit_add_Supplier_box2"
+            className="ud_edit_add_Student_box2"
             type="text"
             value={lastName}
             required
@@ -96,11 +96,11 @@ const UD_AddStudent = (props) => {
               setLastName(e.target.value);
             }}
           />
-          <label className="ud_edit_add_Supplier_name3">
+          <label className="ud_edit_add_Student_name3">
             <b>Address</b>
           </label>
           <input
-            className="ud_edit_add_Supplier_box3"
+            className="ud_edit_add_Student_box3"
             type="text"
             value={address}
             required
@@ -109,11 +109,11 @@ const UD_AddStudent = (props) => {
             }}
           />
           <br></br>
-          <label className="ud_edit_add_Supplier_name4">
+          <label className="ud_edit_add_Student_name4">
             <b>City</b>
           </label>
           <input
-            className="ud_edit_add_Supplier_box4"
+            className="ud_edit_add_Student_box4"
             type="text"
             value={city}
             required
@@ -122,11 +122,11 @@ const UD_AddStudent = (props) => {
             }}
           />
           <br></br>
-          <label className="ud_edit_add_Supplier_name7">
+          <label className="ud_edit_add_Student_name7">
             <b>PhoneNumber</b>
           </label>
           <input
-            className="ud_edit_add_Supplier_box7"
+            className="ud_edit_add_Student_box7"
             type="text"
             value={phoneNumber}
             required
@@ -134,11 +134,11 @@ const UD_AddStudent = (props) => {
               setPhoneNumber(e.target.value);
             }}
           />
-          <label className="ud_edit_add_Supplier_name5">
+          <label className="ud_edit_add_Student_name5">
             <b>EMAIL</b>
           </label>
           <input
-            className="ud_edit_add_Supplier_box5"
+            className="ud_edit_add_Student_box5"
             type="text"
             value={email}
             required
@@ -147,12 +147,12 @@ const UD_AddStudent = (props) => {
             }}
           />
           <br></br>
-          <label className="ud_edit_add_Supplier_name6">
+          <label className="ud_edit_add_Student_name6">
             <b>Password</b>
           </label>
           <br></br>
           <input
-            className="ud_edit_add_Supplier_box6"
+            className="ud_edit_add_Student_box6"
             type="text"
             value={password}
             required
@@ -163,13 +163,13 @@ const UD_AddStudent = (props) => {
           <br></br>
         </form>
 
-        <button className="ud_add_Supplier_Savebutton" onClick={manageStudent}>
+        <button className="ud_add_Student_Savebutton" onClick={manageStudent}>
           Save
         </button>
 
         <div
           onClick={() => props.onClick("manageStudent")}
-          className="ud_add_Supplier_Backbutton"
+          className="ud_add_Student_Backbutton"
         >
           Cancel
         </div>
